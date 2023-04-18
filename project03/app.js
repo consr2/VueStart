92a21910f770042ba86dfeb254ab454e696a209e
consr2 <qqq4861@naver.com>
2023-04-18 18:13:34 +0900

    3-1

--- project03/app.js
+++ project03/app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     //객체를 리턴하는 함수(data 고정)
     data(){
         return{
-            courseGoal: 'aaaa',
+            courseGoalA: 'aaaa',
+            courseGoalB: '<p>bbbbb</p>',
             vueLink: 'http://vuejs.org/'
         };
     },
@@ -10,9 +11,12 @@ const app = Vue.createApp({
         outputGoal(){
             const randomNumber = Math.random();
             if(randomNumber < 0.5){
-                return 'Learn Vue!';
+                return this.courseGoalA;    
+                //원래 자바스크립트 this는 data를 가리키는 것이 아니지만
+                //vue 에서는 createVue라는 객체가 data의 객체를 컨테이너로 등록해 놓기 때문에 접근 가능하게 된다
+                //vue의 특수한 문법이라고 이해
             } else{
-                return 'Master Vue!'
+                return this.courseGoalB;
             }
         }
 
