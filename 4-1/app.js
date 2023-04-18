9d48b6ed38a5db7be213fc43ea2c7f032588cbe5
consr2 <qqq4861@naver.com>
2023-04-18 22:12:44 +0900

    4-1 수정

--- 4-1/app.js
+++ 4-1/app.js
@@ -3,8 +3,31 @@ const app = Vue.createApp({
     return {
       counter: 0,
       input: '',
-      name: ''
+      name: '',
+      fullname: '',
+      familyname:'',
     };
+  },
+  // data에 선언한 속성을 이름으로 선언한 함수
+  // 해당 속성에 변경이 감지되면 실행
+  // 파라미터로 해당 값을 가져옴
+  watch:{
+    counter(value){
+      if(value > 10){
+        this.counter = 0
+      }
+    }
+  },
+  //메서드는 페이지의 변화가 있으면 전부 재실행 되지만
+  //computed는 해당 파라미터의 변경이 감지될 때만 실행함
+  //로딩 부하를 줄여준다.(보편적으로 computed를 사용)
+  computed:{
+    addName(){
+      if(this.name === ''){
+        return '';
+      }
+      return this.name  + this.familyname +  '님 '
+    }
   },
   methods:{
     add(){
